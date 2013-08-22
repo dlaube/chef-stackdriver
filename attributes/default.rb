b81fcb53a056133f342d66631149a7c505ecaddb
Steve Mactaggart <steve.mactaggart@open.edu.au>
2013-08-22 19:42:54 +1000

    Added ubuntu support

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,21 @@
 
 default['stackdriver']['api']['key'] = "YOUR_API_KEY_HERE"
 default['stackdriver']['collectd']['config'] = "yes"
+
+case node[:platform]
+when 'amazon'
+  default[:stackdriver][:repo_url] = 'http://repo.stackdriver.com/stackdriver-amazonlinux.repo'
+when 'redhat', 'centos'
+  default[:stackdriver][:repo_url] = 'http://repo.stackdriver.com/stackdriver.repo'
+
+when 'ubuntu'
+  case node[:platform_version]
+    when '12.04', '12.10'
+      default[:stackdriver][:repo_url] = 'deb http://repo.stackdriver.com/apt precise main'
+    when '10.04'
+      default[:stackdriver][:repo_url] = 'deb http://repo.stackdriver.com/apt lucid main'
+    else
+      default[:stackdriver][:repo_url] = 'deb http://repo.stackdriver.com/apt precise main'
+  end
+
+end
--- recipes/repo.rb
+++ recipes/repo.rb
@@ -8,26 +8,36 @@
 # All rights reserved 
 #
 
-# Re-make the yum cache vi command resource
-execute "create-yum-cache" do
- command "yum -q makecache"
- action :nothing
-end
+case node[:platform]
+when 'rhel', 'centos', 'amazon'
 
-# Reload the yum cache using the Chef provider
-ruby_block "internal-yum-cache-reload" do
-  block do
-    Chef::Provider::Package::Yum::YumCache.instance.reload
+  # Re-make the yum cache vi command resource
+  execute "create-yum-cache" do
+   command "yum -q makecache"
+   action :nothing
   end
-  action :nothing
+
+  # Reload the yum cache using the Chef provider
+  ruby_block "internal-yum-cache-reload" do
+    block do
+      Chef::Provider::Package::Yum::YumCache.instance.reload
+    end
+    action :nothing
+  end
+
+  # Create the StackDriver yum repo file in yum.repos.d
+  cookbook_file "/etc/yum.repos.d/stackdriver.repo" do
+    source "stackdriver.repo"
+    mode 00644
+    notifies :run, "execute[create-yum-cache]", :immediately
+    notifies :create, "ruby_block[internal-yum-cache-reload]", :immediately
+  end
+
+when 'ubuntu'
+
+  # Create the StackDriver apt repo list
+  remote_file "/etc/apt/sources.list.d/stackdriver.list" do
+    source node[:stackdriver][:repo_url]
+  end
+
 end
-
-# Create the StackDriver yum repo file in yum.repos.d
-cookbook_file "/etc/yum.repos.d/stackdriver.repo" do
-  source "stackdriver.repo"
-  mode 00644
-  notifies :run, "execute[create-yum-cache]", :immediately
-  notifies :create, "ruby_block[internal-yum-cache-reload]", :immediately
-end
-
-
